f4dd1f3446a0ff59d3d31e203524345dd44473c2
arsan <arsan_01@protonmail.com>
2025-10-20 15:23:36 +0600

    add feed to posts

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import Post, Like, Comment
 from .serializers import PostSerializer, LikeSerializer, CommentSerializer
+from apps.friends.models import Friendship, Follow
+from django.db.models import Q, F, When, Case, Value
+
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
     """Tolko avtor mojet redaktirovat ili udalyat` posty"""
@@ -60,3 +63,48 @@ class PostViewSet(viewsets.ModelViewSet):
             serializer.is_valid(raise_exception=True)
             serializer.save(post=post)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+    @action(detail=False, methods=['get'], url_path='feed')
+    def feed(self, request):
+        user = request.user
+
+        friend_ids = Friendship.objects.filter(
+            Q(user1=user) | Q(user2=user)
+        ).values_list('user1_id', 'user2_id')
+
+        friends_set = {uid for pair in friend_ids
+                       for uid in pair if uid != user.id}
+        
+        following_ids = set(
+            Follow.objects.filter(follower=user).values_list('following_id', flat=True)
+        )
+
+        visible_authors = friends_set | following_ids | {user.id}
+
+        posts = Post.objects.filter(
+            author_id__in=visible_authors,
+            is_public=True
+        ).select_related('author').order_by('-created_at')
+
+        page = self.paginate_queryset(posts)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # flat_firends_ids = set()
+        # for pair in friend_ids:
+        #     flat_firends_ids.update(pair)
+        # flat_firends_ids.discard(user.id)
+
+        # following_ids = Follow.objects.filter(follower=user).values_list('following_id', flat=True)
+        # visible_authors = set(flat_firends_ids) | set(following_ids) | {user.id}
+
+        # posts = (
+        #     Post.objects.filter(author_id__in=visible_authors).select_related('author').order_by('-created_at')
+        # )
+
+        # serializer = self.get_serializer(posts, many=True)
+        # return Response(serializer.data, status=status.HTTP_200_OK)
